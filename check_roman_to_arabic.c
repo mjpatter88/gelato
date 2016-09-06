28690dc8ba854a801f14e276a69dff250c29764a
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-05 20:09:26 -0500

    Add test to convert II to 2. This will drive in the actual conversion logic.

--- check_roman_to_arabic.c
+++ check_roman_to_arabic.c
@@ -12,6 +12,15 @@ START_TEST(rtoa_one)
 }
 END_TEST
 
+START_TEST(rtoa_two)
+{
+    int returned_num = 0;
+    int expected_num = 2;
+    returned_num = roman_to_arabic("II");
+    ck_assert_int_eq(returned_num, expected_num);
+}
+END_TEST
+
 Suite *make_roman_to_arabic_suite(void)
 {
     Suite *test_suite = suite_create("RomanToArabic");
@@ -20,6 +29,7 @@ Suite *make_roman_to_arabic_suite(void)
     suite_add_tcase(test_suite, rtoa_test_case);
 
     tcase_add_test(rtoa_test_case, rtoa_one);
+    tcase_add_test(rtoa_test_case, rtoa_two);
 
     return test_suite;
 }
