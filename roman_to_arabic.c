65b860ad2f74626b5710a0c91f916db0b7639747
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-10 22:41:11 -0500

    Implement conversion of V to 5. Use #defines for now, but research a better rstructure.

--- roman_to_arabic.c
+++ roman_to_arabic.c
@@ -2,10 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define ROM_ONE 'I'
+#define ROM_FIVE 'V'
 
 int roman_to_arabic(char *roman_numeral)
 {
+    int arabic = 0;
     size_t len = strlen(roman_numeral);
-    return len;
+    int i = 0;
+    for(i=len-1; i>=0; i--)
+    {
+        switch(roman_numeral[i])
+        {
+            case ROM_ONE:
+                arabic += 1;
+                break;
+            case ROM_FIVE:
+                arabic += 5;
+                break;
+            default:
+                break;
+        }
+
+    }
+    return arabic;
 }
