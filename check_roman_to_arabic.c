f543b8e872e8a1ed50d27eedb2ffa8a6e106db50
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-11 18:33:04 -0500

    Add some more roman numeral to arabic numeral conversion tests.

--- check_roman_to_arabic.c
+++ check_roman_to_arabic.c
@@ -57,6 +57,15 @@ START_TEST(rtoa_fifty)
 }
 END_TEST
 
+START_TEST(rtoa_sixty_seven)
+{
+    int returned_num = 0;
+    int expected_num = 67;
+    returned_num = roman_to_arabic("LXVII");
+    ck_assert_int_eq(returned_num, expected_num);
+}
+END_TEST
+
 START_TEST(rtoa_one_hundred)
 {
     int returned_num = 0;
@@ -84,6 +93,15 @@ START_TEST(rtoa_one_thousand)
 }
 END_TEST
 
+START_TEST(rtoa_two_thousand_three_hundred_twenty_one)
+{
+    int returned_num = 0;
+    int expected_num = 2321;
+    returned_num = roman_to_arabic("MMCCCXXI");
+    ck_assert_int_eq(returned_num, expected_num);
+}
+END_TEST
+
 Suite *make_roman_to_arabic_suite(void)
 {
     Suite *test_suite = suite_create("RomanToArabic");
@@ -97,9 +115,11 @@ Suite *make_roman_to_arabic_suite(void)
     tcase_add_test(rtoa_test_case, rtoa_five);
     tcase_add_test(rtoa_test_case, rtoa_ten);
     tcase_add_test(rtoa_test_case, rtoa_fifty);
+    tcase_add_test(rtoa_test_case, rtoa_sixty_seven);
     tcase_add_test(rtoa_test_case, rtoa_one_hundred);
     tcase_add_test(rtoa_test_case, rtoa_five_hundred);
     tcase_add_test(rtoa_test_case, rtoa_one_thousand);
+    tcase_add_test(rtoa_test_case, rtoa_two_thousand_three_hundred_twenty_one);
 
     return test_suite;
 }
