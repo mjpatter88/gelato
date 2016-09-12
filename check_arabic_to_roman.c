fef1e2e8dd6913322f49f60dff5461d39c632923
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-11 19:52:50 -0500

    Test 50 to L.

--- check_arabic_to_roman.c
+++ check_arabic_to_roman.c
@@ -54,6 +54,16 @@ START_TEST(ator_ten)
 }
 END_TEST
 
+START_TEST(ator_fifty)
+{
+    char roman_numeral[MAX_ROMAN_NUMERAL_LENGTH];
+    char expected_roman_numeral[] = "L";
+    int return_code = arabic_to_roman(50, roman_numeral);
+    ck_assert_int_eq(return_code, 0);
+    ck_assert_str_eq(roman_numeral, expected_roman_numeral);
+}
+END_TEST
+
 Suite *make_arabic_to_roman_suite(void)
 {
     Suite *test_suite = suite_create("ArabicToRoman");
@@ -66,6 +76,7 @@ Suite *make_arabic_to_roman_suite(void)
     tcase_add_test(ator_test_case, ator_three);
     tcase_add_test(ator_test_case, ator_five);
     tcase_add_test(ator_test_case, ator_ten);
+    tcase_add_test(ator_test_case, ator_fifty);
 
     return test_suite;
 }
