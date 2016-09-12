82bc00be3f97d881a819fd24e13f3f16882f2fa4
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-11 20:00:13 -0500

    Test 1000 to M.

--- check_arabic_to_roman.c
+++ check_arabic_to_roman.c
@@ -84,6 +84,16 @@ START_TEST(ator_five_hundred)
 }
 END_TEST
 
+START_TEST(ator_one_thousand)
+{
+    char roman_numeral[MAX_ROMAN_NUMERAL_LENGTH];
+    char expected_roman_numeral[] = "M";
+    int return_code = arabic_to_roman(1000, roman_numeral);
+    ck_assert_int_eq(return_code, 0);
+    ck_assert_str_eq(roman_numeral, expected_roman_numeral);
+}
+END_TEST
+
 Suite *make_arabic_to_roman_suite(void)
 {
     Suite *test_suite = suite_create("ArabicToRoman");
@@ -99,6 +109,7 @@ Suite *make_arabic_to_roman_suite(void)
     tcase_add_test(ator_test_case, ator_fifty);
     tcase_add_test(ator_test_case, ator_one_hundred);
     tcase_add_test(ator_test_case, ator_five_hundred);
+    tcase_add_test(ator_test_case, ator_one_thousand);
 
     return test_suite;
 }
