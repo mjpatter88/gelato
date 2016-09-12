997373d63934c0a372d1e1ed1bdac4f655baeb2e
Michael Patterson <mpatterson@pillartechnology.com>
2016-09-11 19:42:18 -0500

    Implement 2 to II.

--- arabic_to_roman.c
+++ arabic_to_roman.c
@@ -1,7 +1,18 @@
+#include <stdlib.h>
 #include "arabic_to_roman.h"
+#include "gelato.h"
+
+#define ROM_ONE 'I'
+
 int arabic_to_roman(int arabic_numeral, char *roman_numeral)
 {
-    char rom[] = "I";
-    strncpy(roman_numeral, rom, sizeof(rom));
+    memset(roman_numeral, 0, MAX_ROMAN_NUMERAL_LENGTH);
+    size_t index = 0;
+    while(arabic_numeral > 0)
+    {
+        roman_numeral[index] = 'I';
+        index++;
+        arabic_numeral--;
+    }
     return 0;
 }
